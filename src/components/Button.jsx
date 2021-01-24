c94c5a545f2a91edeca3a295ae1d490ed0d72152
josealves380 <netopereira464@gmail.com>
2021-01-24 13:07:38 -0300

    Refatorando e aplicando funções e estilos nos botões da calculadora

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './Button.css'
 
 export default props => {
+
     let classes = 'button '
-    classes += props.operation ? 'opertaion' : ''
+    classes += props.operation ? 'operation' : ''
     classes += props.double ? 'double' : ''
     classes += props.triple ? 'triple' : ''
 
