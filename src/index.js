e81e0e0e6eb74f362864eecf7b3b1dbf574fb668
josealves380 <netopereira464@gmail.com>
2021-01-24 18:08:36 -0300

    Adicionando funções para realizar operações matematicas na calculadora e corrigindo alguns bugs de estilização

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <div>
     <h1>Calculadora</h1>
     <Calculator />
-  </React.StrictMode>,
+  </div>,
   document.getElementById('root')
 );
 
