7812c0dba57785efb779f129496e9022e834e6f7
josealves380 <netopereira464@gmail.com>
2021-01-24 11:34:17 -0300

    Estilo para os botões e funções

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import './Button.css'
 
-export default props =>
-    <button className='button'>{props.label}</button>
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'opertaion' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button
+            onclick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
